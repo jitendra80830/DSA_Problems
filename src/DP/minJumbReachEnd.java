8d128a35a7e4a466383930377977dc6e1b569061
jitendra <jitendrakumar89973@gmail.com>
2022-04-27 07:49:52 +0530

    DSA

---
+++ src/DP/minJumbReachEnd.java
@@ -0,0 +1,44 @@
+package DP;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class minJumbReachEnd {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        for (int i = 0; i <n ; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int dp[] = new int[n];
+        Arrays.fill(dp ,Integer.MAX_VALUE);
+         System.out.println(minJumpReachEnd(arr , dp ,n));
+    }
+
+    private static int minJumpReachEnd(int[] arr, int[] dp, int n) {
+        if(arr[0] == 0){
+            return Integer.MAX_VALUE;
+        }
+        dp[0] = 0;
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j <i ; j++) {
+                if(dp[j]!=Integer.MAX_VALUE && arr[j]+j>=i){
+                    if(dp[j] + 1 < dp[i]){
+                        dp[i] = dp[j]+1;
+                    }
+                }
+//                if(i <= (j + arr[j])){
+//                    dp[i] = Math.min(dp[i] , dp[j]+1);
+//                }
+
+            }
+
+        }
+        if(dp[n-1]!=Integer.MAX_VALUE){
+            return dp[n-1];
+        }
+        return -1;
+
+    }
+}
--- src/StringAlgorithms/Rabin_karp_StringMatching.java
+++ src/StringAlgorithms/Rabin_karp_StringMatching.java
@@ -7,15 +7,43 @@ public class Rabin_karp_StringMatching {
         Scanner sc = new Scanner(System.in);
         String T = sc.next();
         String P = sc.next();
-        int d = 26;
+        int d = 26; //redix
         int q = 1000000007;
-        rabin_kaep_Algo(P , T , d , q);
+        rabin_karp_Algo(P , T , d , q);
 
     }
 
-    private static void rabin_kaep_Algo(String P, String T, int d, int q) {
+    private static void rabin_karp_Algo(String P, String T, int d, int q) {
         int n = T.length();
         int m  = P.length();
+        int h = (int)Math.pow(d ,m-1);
+        int p = 0;
+        int t[] = new int[n-1+1];
+        for (int i = 0; i <m ; i++) {
+            p  = (d*p + (P.charAt(i) - 'a'))%q;
+            t[0] = (d*t[0] +(T.charAt(i) - 'a'))%q;
 
+        }
+        for (int s = 0; s <=n-m ; s++) {
+            if(p == t[s]){
+                if(charMatching(T,P,s,m)){
+                    System.out.println("character matched after "+s+" shifts");
+                }
+            }
+            if(s < n-m){
+                t[s+1] =(d*(t[s] - (T.charAt(s) - 'a') *h) + (T.charAt(s+m) - 'a'))%q;
+            }
+
+        }
+
+    }
+
+    private static boolean charMatching(String T, String P, int s, int m) {
+        for (int j = 0; j <m ; j++) {
+            if(P.charAt(j)!=T.charAt(j+s)){
+                return false;
+            }
+        }
+        return true;
     }
 }
